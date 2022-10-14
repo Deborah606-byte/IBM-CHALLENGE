57061581f8ee50f292bd38fef5dae1fc2d881228
Nyamadi Atsu <devbrainy98@gmail.com>
2022-10-14 14:36:05 +0100

    merged changes and added more sections

--- public/index.js
+++ public/index.js
@@ -1,14 +1,15 @@
 // Load browser before Js runs
 window.addEventListener('DOMContentLoaded', () => {
-// Declaring variables
-const hamburgerMenu = document.querySelector('#hamburger-menu');
-const mobileNav = document.querySelector('#mobile-nav');
+    // Declaring variables
+    const hamburgerMenu = document.querySelector('#hamburger-menu');
+    const mobileNav = document.querySelector('#mobile-nav');
 
-// Adding event listener and functionalities
-hamburgerMenu.addEventListener('click', () => {
-    mobileNav.classList.toggle('translate-y-[0]');
-    hamburgerMenu.classList.toggle('bi-x-lg');
-})
-
+    // Adding event listener and functionalities
+    hamburgerMenu.addEventListener('click', () => {
+        ['translate-y-[-100rem]', 'translate-y-[0]'].map(item => {
+            mobileNav.classList.toggle(item);
+        })
+        hamburgerMenu.classList.toggle('bi-x-lg');
+    });
 
 })
