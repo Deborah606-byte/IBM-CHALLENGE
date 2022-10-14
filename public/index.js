ca8b3287e673453337208a5288cdf4c150e8e042
Nyamadi Atsu <devbrainy98@gmail.com>
2022-10-14 14:41:44 +0100

    added dynamic date in js & modified index.html

--- public/index.js
+++ public/index.js
@@ -3,6 +3,10 @@ window.addEventListener('DOMContentLoaded', () => {
     // Declaring variables
     const hamburgerMenu = document.querySelector('#hamburger-menu');
     const mobileNav = document.querySelector('#mobile-nav');
+    const date = document.querySelector('#date');
+
+    // Getting the current year
+    date.innerHTML = new Date().getFullYear();
 
     // Adding event listener and functionalities
     hamburgerMenu.addEventListener('click', () => {
